8f13e1ce9d69c382630f8d8f8448ef8edf3f9012
vorogurcov <vania.frol2010@yandex.ru>
2024-02-21 12:23:58 +0300

    Developed Brute Force version of the program

--- lab1/Lab1/Lab1/FuncsLib.cpp
+++ lab1/Lab1/Lab1/FuncsLib.cpp
@@ -1,1 +1,28 @@
 #include "Header.h"
+
+
+int Amount = 0;
+
+int FindAmount(int const n, int const k)
+{
+	
+	if (k == 0)
+	{
+		return ++Amount;
+	}
+	else if (k < 0)
+		return 0;
+	else if (k > 0 && n == 0)
+		return 0;
+
+	for (size_t num = 0; num < 10; num++)
+	{
+		int remainder = k - num;
+		FindAmount(n - 1, remainder);
+	}
+}
+
+void GetAmount()
+{
+	printf("%d", Amount);
+}
--- lab1/Lab1/Lab1/main.cpp
+++ lab1/Lab1/Lab1/main.cpp
@@ -1,6 +1,16 @@
 #include "Header.h"
 
+
+extern int FindAmount(int const n, int const k);
+extern void GetAmount();
+
 int main(int argc, char* argv)
 {
+	//Interface -> n
+	int n = 3;
+	int k = 1;
+	
 
+	FindAmount(n, k);
+	GetAmount();
 }
