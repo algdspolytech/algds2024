824241af135c086728776ccb8dbba14f91257307
vorogurcov <vania.frol2010@yandex.ru>
2024-02-22 19:20:21 +0300

    Optimized program. Changed Brute Force solution.

--- lab1/Lab1/Lab1/FuncsLib.cpp
+++ lab1/Lab1/Lab1/FuncsLib.cpp
@@ -1,5 +1,29 @@
 #include "Header.h"
 
+long long** CreateMatrix(int const n, int const k)
+{
+	long long** NewMatrix = (long long**)malloc((n + 1) * sizeof(long long*));
+	for (size_t i = 0; i < n + 1; i++)
+	{
+		NewMatrix[i] = (long long*)malloc((k + 1)*sizeof(long long));
+		for (size_t j = 0; j < k + 1; j++)
+			NewMatrix[i][j] = -1;
+	}
+	return NewMatrix;
+}
+
+void ShowMatrix(long long** matr,int n, int k)
+{
+	for (size_t i = 0; i < n+1; i++)
+	{
+		for (size_t j = 0; j < k+1; j++)
+		{
+			printf("%d ", matr[i][j]);
+		}
+		putchar('\n');
+	}
+}
+
 int IsInMemo(int const n, int const remainder, memo const Memo)
 {
 	if (Memo.n == n && Memo.remainder == remainder)
@@ -15,63 +39,73 @@ FullMemo* InitFullMemo()
 	NewMemo->MemoSize = 0;
 	return NewMemo;
 }
-
-memo* Finder(int const n, int const remainder, FullMemo* const Memo)
-{
-	for (size_t i = 0; i < Memo->MemoSize; i++)
-	{
-		if (IsInMemo(n, remainder, Memo->MemoArray[i]))
-			return &(Memo->MemoArray[i]);
-		else
-			return NULL;
-	}
-}
+//
+//memo* Finder(int const n, int const remainder)
+//{
+//	for (size_t i = 0; i < n; i++)
+//	{
+//		for (size_t j = 0; j < remainder; j++)
+//		{
+//			
+//		}
+//	}
+//}
 
 void AddToMemo(int const n, int const remainder, int const Amount, FullMemo* Memo)
 {
-	Memo->MemoArray = (memo*)realloc(Memo->MemoArray, (Memo->MemoSize + 1)*sizeof(memo));
-	Memo->MemoSize++;
+	Memo->MemoArray = (memo*)realloc(Memo->MemoArray, (++Memo->MemoSize)*sizeof(memo));
+
 	Memo->MemoArray[Memo->MemoSize - 1].Amount = Amount;
 	Memo->MemoArray[Memo->MemoSize - 1].remainder = remainder;
 	Memo->MemoArray[Memo->MemoSize - 1].n = n;
 }
 
-int Amount = 0;
-
-int FindAmount(int const n, int const k, FullMemo* Memo)
+void ZeroFill(long long* const arr, int const Size)
 {
-	memo* FoundedMemo = Finder(n,k,Memo);
-	if (FoundedMemo)
-	{
-		Amount += FoundedMemo->Amount;
-		return Amount;
-	}
+	for (size_t i = 0; i < Size; i++)
+		arr[i] = 0;
+}
+
+long long GetSum(long long* const arr, int const Size)
+{
+	long long summ = 0;
+	for (size_t i = 0; i < Size; i++)
+		summ += arr[i];
+	return summ;
+}
+
+long long FindAmount(int const n, int const k, long long** HelperMatrix)
+{
+	
+
 	if (k == 0)
 	{
-		return ++Amount;
+		return 1;
 	}
 	else if (k < 0)
 		return 0;
 	else if (k > 0 && n == 0)
 		return 0;
 
-	
+	if (HelperMatrix[n][k] != -1)
+		return HelperMatrix[n][k];
+
+	long long Amount[10];
+	ZeroFill(Amount, 10);
 	for (size_t num = 0; num < 10; num++)
 	{
 		int remainder = k - num;
-		if (remainder > 9 * n)
+		if (remainder > 9 * (n - 1))
 		{
-			AddToMemo(n, remainder, 0, Memo);
+			HelperMatrix[n - 1][remainder] = 0;
 			continue;
 		}
-		int temp = Amount;
-		FindAmount(n - 1, remainder, Memo);
-		if (temp != Amount)
-				AddToMemo(n,remainder,Amount,Memo);
+		
+		Amount[num] = FindAmount(n - 1, remainder, HelperMatrix);
+		if (Amount[num])
+			HelperMatrix[n - 1][remainder] = Amount[num];
+		
 	}
+	return GetSum(Amount,10);
 }
 
-void GetAmount()
-{
-	printf("%d", Amount);
-}
--- lab1/Lab1/Lab1/main.cpp
+++ lab1/Lab1/Lab1/main.cpp
@@ -1,16 +1,19 @@
 #include "Header.h"
 
 
-extern int FindAmount(int const n, int const k, FullMemo* Memo);
-extern void GetAmount();
+extern long long FindAmount(int const n, int const k, long long** HelperMatrix);
 extern FullMemo* InitFullMemo();
+extern long long** CreateMatrix(int const n, int const k);
+extern void ShowMatrix(long long** matr, int n, int k);
 
 int main(int argc, char* argv)
 {
 	//Interface -> n
-	int n = 10;
-	int k = 90;
-	FullMemo* Memo = InitFullMemo();
-	FindAmount(n, k, Memo);
-	GetAmount();
+	int n = 1213;
+	int k = 2;//4426837690959633216
+			   
+	long long** HelperMatrix = CreateMatrix(n, k);
+
+	 // 1287484(10/15) ... 432457640(10/45)-3.28 opt - 7.00 notopt
+	printf("%lld\n",FindAmount(n, k, HelperMatrix));
 }
