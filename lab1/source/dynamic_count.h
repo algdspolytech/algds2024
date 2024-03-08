0cb13a7c9c088cc22909d9affa97ae1932c572a5
Forthey <0forthey0@gmail.com>
2024-03-08 04:26:00 +0300

    Nothing here...

--- lab1/source/dynamic_count.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-typedef int** DynamicTable;
-
-int ñountNumbers(int n, int k);
--- lab1/source/tests/trivial.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-typedef unsigned char Digit;
-typedef Digit* Number;
-
-int countNumberViaEnum(int n, int k);
-
