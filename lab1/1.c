d7e76c1c0716f2dd1ab1317e5944f85a7e240c24
Michail14567 <mikhail_spiridonov_0@mail.ru>
2024-03-08 19:22:07 +0300

    1

--- lab1/1.c
+++ lab1/1.c
@@ -1,1 +1,42 @@
-test;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+char* findLCS(char *X, char *Y, int m, int n) {
+    int L[m+1][n+1];
+    int i, j;
+
+    for (i=0; i<=m; i++) {
+        for (j=0; j<=n; j++) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (X[i-1] == Y[j-1])
+                L[i][j] = L[i-1][j-1] + 1;
+            else
+                L[i][j] = max(L[i-1][j], L[i][j-1]);
+        }
+    }
+
+    int index = L[m][n];
+    char* lcs = (char*)malloc((index+1) * sizeof(char));
+    lcs[index] = '\0';
+
+    i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (X[i-1] == Y[j-1]) {
+            lcs[index-1] = X[i-1];
+            i--; j--; index--;
+        }
+        else if (L[i-1][j] > L[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+    return lcs;
+}
+
---
+++ lab1/test1.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern "C" {
+    #include "findLCS.h" // Предполагается, что ваш findLCS.h содержит объявления функций findLCS и max
+}
+
+// Тесты для функции findLCS
+TEST(LCSTest, BasicTest) {
+    char X[] = "10101";
+    char Y[] = "10011";
+    int m = strlen(X);
+    int n = strlen(Y);
+    char* result = findLCS(X, Y, m, n);
+    ASSERT_STREQ(result, "1001");
+    free(result); // Очистка памяти после каждого теста
+}
+
+TEST(LCSTest, TestEmptyString) {
+    char X[] = "";
+    char Y[] = "10011";
+    int m = strlen(X);
+    int n = strlen(Y);
+    char* result = findLCS(X, Y, m, n);
+    ASSERT_STREQ(result, "");
+    free(result);
+}
+
+// Тесты для функции max
+TEST(MaxTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(5, max(3, 5));
+    EXPECT_EQ(10, max(10, 2));
+}
+
+TEST(MaxTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(-1, max(-1, -5));
+    EXPECT_EQ(-2, max(-2, -10));
+}
+
+TEST(MaxTest, HandlesZero) {
+    EXPECT_EQ(0, max(0, -5));
+    EXPECT_EQ(5, max(0, 5));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
