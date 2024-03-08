012bc697e6b6a2092daf299641e914714dca26b7
EugenDid <108171626+NEurosgen@users.noreply.github.com>
2024-03-08 10:15:17 +0300

    Add files via upload

--- lab1/Source.c
+++ lab1/Source.c
@@ -2,43 +2,36 @@
 #include <assert.h>
 #include <stdlib.h> 
 #include "Header.h"
-void ans(int A, int n) {
+int* ans(int A, int n) {
 
 	int* b;
 	int* c;
 	int* ans;
 	int** mas;
-	ans = malloc(sizeof(int) * n);
+	ans = calloc (n,sizeof(int));
 	b = getarray(n);
 	c = getarray(n);
 	mas = creatematrix(b, c, A, n);
 	findans(n, A, mas, b,ans);
+	for (int i = 0; i <= n; ++i) {
+		free(mas[i]);
+	}
+	free(mas);
+	free(b);
+	free(c);
+	return ans;
 }
 int main() {
 
-#if 0
+#
 	int A;
 	int n;
+	int* res;
 	scanf("%d %d", &A, &n);
-	ans(A, n);
-#endif	
-	int b[5] = { 3,4 };
-	int c[5] = { 1,6 };
-	int** mas = creatematrix(&b[0], &c[0], 2, 2);
-	int ans[2] = { 0 };
-	findans(2, 2, (const int**)mas, &b[0], &ans[0]);
-	for (int i = 0; i <= 2; ++i) {
-		for (int j = 0; j <= 3; ++j) {
-			printf("%d ", mas[i][j]);
-		}
-		printf("\n");
-	}
+	
+	res = ans(A, n);
 
-	
-	for (int i = 0; i < 2; ++i) {
-		printf("%d ", ans[i]);
-	}
-	
+
 
 	
 }
