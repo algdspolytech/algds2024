367b934321c4bcfcd3a0227a6b630999a7ff6493
efim <efimburachevsky@gmail.com>
2024-03-13 13:10:10 +0300

    init

---
+++ lab1/helpFunctions.c
@@ -0,0 +1,9 @@
+#include "helpFunctions.h"
+
+int min(int a, int b) {
+    return a < b ? a : b; // Функция для нахождения минимума
+}
+
+void printMatrix(int m, int n, int** A) {
+    printf("Streets:\n\n");
+}
---
+++ lab1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include "helpFunctions.h" 
+
+int main() {
+    
+    return 0;
+}
