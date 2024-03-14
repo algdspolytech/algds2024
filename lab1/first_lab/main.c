e84597181c9d6df812aaabf42cc47568132e6d99
1Sorangar1 <ragnarchern@mail.ru>
2024-03-14 20:46:26 +0300

    First commit

---
+++ lab1/first_lab/main.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+
+	FILE* f;
+	f = fopen("input.txt", "r");
+	if (f == NULL) {
+		printf("Errors occured while reading file");
+		exit(0);
+	}
+
+	int n;
+	fscanf(f, "%d", &n);
+
+	if (n == 0) {
+		printf("No sequence is given");
+		exit(0);
+	}
+	int* list = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		fscanf(f, "%d", &list[i]);
+	}
+	fclose(f);
+
+
+	int start = 0, finish = 0, max_sum = 1, current_sum = 1, max_start = 0, max_finish = 0;
+	for (int i = 0; i < n; i++) {
+		if (list[i + 1] % list[i] == 0) {
+			current_sum += 1;
+			finish = i + 1;
+		}
+		else {
+			if (current_sum > max_sum) {
+				max_sum = current_sum;
+				max_start = start;
+				max_finish = finish;
+			}
+			current_sum = 1;
+			start = i + 1;
+			finish = i + 1;
+
+		}
+	}
+
+	int* result = (int*)malloc(max_sum * sizeof(int));
+	for (int i = max_start; i <= max_finish; i++) {
+		result[i - max_start] = list[i];
+	}
+
+	for (int i = 0; i < max_sum; i++) {
+		printf("%d ", result[i]);
+	}
+
+	return 0;
+}
