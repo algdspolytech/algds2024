7acdd557224f513ad8e3201e3b6ffc088b0a780d
1Sorangar1 <ragnarchern@mail.ru>
2024-03-15 00:16:58 +0300

    Function appearing

--- lab1/first_lab/main.c
+++ lab1/first_lab/main.c
@@ -1,33 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-
-int main() {
-
-	FILE* f;
-	f = fopen("input.txt", "r");
-	if (f == NULL) {
-		printf("Errors occured while reading file");
-		exit(0);
-	}
-
-	int n;
-	fscanf(f, "%d", &n);
-
-	if (n == 0) {
-		printf("No sequence is given");
-		exit(0);
-	}
-	int* list = (int*)malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++) {
-		fscanf(f, "%d", &list[i]);
-	}
-	fclose(f);
-
+void function(int const* list, int const n) {
 
 	int start = 0, finish = 0, max_sum = 1, current_sum = 1, max_start = 0, max_finish = 0;
+
 	for (int i = 0; i < n; i++) {
 		if (list[i + 1] % list[i] == 0) {
 			current_sum += 1;
@@ -54,6 +32,34 @@ int main() {
 	for (int i = 0; i < max_sum; i++) {
 		printf("%d ", result[i]);
 	}
+
+	return 0;
+}
+
+int main() {
+
+	FILE* f;
+	f = fopen("input.txt", "r");
+	if (f == NULL) {
+		printf("Errors occured while reading file");
+		exit(0);
+	}
+
+	int n;
+	fscanf(f, "%d", &n);
+
+	if (n == 0) {
+		printf("No sequence is given");
+		exit(0);
+	}
+	int* list = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		fscanf(f, "%d", &list[i]);
+	}
+	fclose(f);
+
+	function(list, n);
+
 
 	return 0;
 }
