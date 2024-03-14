a879168844fa575e4a31ff84c4cdc821ec195c05
1Sorangar1 <ragnarchern@mail.ru>
2024-03-15 01:56:48 +0300

    Structure appearing + func division

--- lab1/first_lab/main.c
+++ lab1/first_lab/main.c
@@ -1,8 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+//#include <cassert>
 
-void function(int const* list, int const n) {
+typedef struct list_s {
+	int* seq;
+	int N;
+}list_s;
+
+
+list_s* readfile(char const* filename) {
+
+	list_s* result = (list_s*)malloc(sizeof(list_s));
+	FILE* f;
+
+	f = fopen(filename, "r");
+	if (f == NULL) {
+		printf("Errors occured while reading file");
+		exit(0);
+	}
+
+	int n;
+	fscanf(f, "%d", &n);
+
+	if (n == 0) {
+		printf("No sequence is given");
+		exit(0);
+	}
+	int* list = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		fscanf(f, "%d", &list[i]);
+	}
+
+	result->N = n;
+	result->seq = list;
+
+	fclose(f);
+
+	return result;
+}
+
+
+
+list_s* function(int const* list, int const n) {
+
+	list_s* result = (list_s*)malloc(sizeof(list_s));
 
 	int start = 0, finish = 0, max_sum = 1, current_sum = 1, max_start = 0, max_finish = 0;
 
@@ -24,42 +66,25 @@ void function(int const* list, int const n) {
 		}
 	}
 
-	int* result = (int*)malloc(max_sum * sizeof(int));
+	int* res = (int*)malloc(max_sum * sizeof(int));
 	for (int i = max_start; i <= max_finish; i++) {
-		result[i - max_start] = list[i];
+		res[i - max_start] = list[i];
 	}
 
-	for (int i = 0; i < max_sum; i++) {
-		printf("%d ", result[i]);
-	}
+	result->N = max_sum;
+	result->seq = res;
 
-	return 0;
+	return result;
 }
 
 int main() {
+	list_s* list = readfile("input.txt");
 
-	FILE* f;
-	f = fopen("input.txt", "r");
-	if (f == NULL) {
-		printf("Errors occured while reading file");
-		exit(0);
+	list_s* result = function(list->seq, list->N);
+
+	for (int i = 0; i < result->N; i++) {
+		printf("%d ", result->seq[i]);
 	}
-
-	int n;
-	fscanf(f, "%d", &n);
-
-	if (n == 0) {
-		printf("No sequence is given");
-		exit(0);
-	}
-	int* list = (int*)malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++) {
-		fscanf(f, "%d", &list[i]);
-	}
-	fclose(f);
-
-	function(list, n);
-
 
 	return 0;
 }
