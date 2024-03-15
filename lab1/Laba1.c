26d7c5881ed557189595dd69a10ca3e13b9ae349
Kotanikach <133512784+Kotanikach@users.noreply.github.com>
2024-03-15 18:50:21 +0300

    Add files via upload

--- lab1/Laba1.c
+++ lab1/Laba1.c
@@ -2,7 +2,6 @@
 #include <locale.h>
 #include <math.h>
 #include <string.h>
-//#define _CRT_SECURE_NO_WARNINGS
 
 int CountSum(int num, int k) {
 	int sum = 0;
@@ -16,66 +15,37 @@ int CountSum(int num, int k) {
 	return sum == k;
 }
 
-//void PrintNumb(int n, int k) {
-//
-//	int raz = 1, maxi = pow(10, n) - 1;
-//
-//	for (int i = 1; i <= maxi; ++i) {
-//		if (i % (int)pow(10, raz) == 0) {
-//			raz += 1;
-//		}
-//		if (CountSum(i, k)) {
-//			for (int j = 0; j < n - raz; ++j) {
-//				printf("0");
-//			}
-//			printf("%d\n", i);
-//		}
-//	}
-//}
-
-void PrintNumber(double n, double k) {
-	if ((floor(n) == n && (int)n == (char)n) && (floor(k) == k && (int)k == (char)k) && n > 0 && k > 0) {
-		int raz = 1, maxi = pow(10, (int)n) - 1;
-
-		for (int i = 1; i <= maxi; ++i) {
-			if (i % (int)pow(10, raz) == 0) {
-				raz += 1;
+int PrintNumber(int n, int k) {
+	int raz = 1, maxi = (int)pow(10, n) - 1;
+	int countnumber = 0;
+	for (int i = 1; i <= maxi; ++i) {
+		if (i % (int)pow(10, raz) == 0) {
+			raz += 1;
+		}
+		if (CountSum(i, k)) {
+			countnumber += 1;
+			for (int j = 0; j < n - raz; ++j) {
+				printf("0");
 			}
-			if (CountSum(i, (int)k)) {
-				for (int j = 0; j < (int)n - raz; ++j) {
-					printf("0");
-				}
-				printf("%d\n", i);
-			}
+			printf("%d ", i);
 		}
 	}
+	return countnumber;
+}
+
+int ReadNumber(double n, double k) {
+	setlocale(LC_ALL, "Rus");
+	if ((floor(n) == n && (int)n == (char)n) && (floor(k) == k && (int)k == (char)k) && n > 0 && k > 0) {
+		PrintNumber(n, k);
+	}
 	else {
-		printf("Введите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
+		printf("Введите корректные данные: <значность сумма> (целые положительные числа большие 0)\n");
+		return 0;
 	}
 }
 
 //int main() {
-//	setlocale(LC_ALL, "Rus");
-//	PrintNumber(3, 6);
-//	PrintNumber(1.2, 4);
-//	PrintNumber('hd', 'j');
-//}
-
-//int main() {
-//	int n, k;
-//	setlocale(LC_ALL, "Rus");
-//
-//	printf("Значность чисел и сумма: ");
-//
-//	while (1) {
-//		if (scanf("%d %d", &n, &k) == 2 && n > 0 && k > 0) {
-//			break;
-//		}
-//		else {
-//			int c;
-//			while ((c = getchar()) != '\n' && c != EOF);
-//			printf("Введите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-//		}
-//	}
-//	PrintNumb(n, k);
+//	ReadNumber(3, 6);
+//	ReadNumber(1.2, 4);
+//	ReadNumber('hd', 'j');
 //}
--- lab1/test.cpp
+++ lab1/test.cpp
@@ -1,84 +1,28 @@
 #include "pch.h"
 #include "gtest/gtest.h"
-#include "../Laba1.c"
-
-//extern "C" {
 //#include "../Laba1.c"
-//}
 
-TEST(PrintNumberTest, Positive) {
-    testing::internal::CaptureStdout();
-    PrintNumber(3, 6);
-    std::string output = testing::internal::GetCapturedStdout();
-	EXPECT_EQ(output, "006\n015\n024\n033\n042\n051\n060\n105\n114\n123\n132\n141\n150\n204\n213\n222\n231\n240\n303\n312\n321\n330\n402\n411\n420\n501\n510\n600\n");
+extern "C" {
+#include "../Laba1.c"
 }
 
-TEST(PrintNumberTest, Negative) {
-    testing::internal::CaptureStdout();
-    PrintNumber(-2, 5);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(-10, -6);
-    std::string output2 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output2, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(4, -2);
-    std::string output3 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output3, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
+TEST(CountSumTest, Correct) {
+    EXPECT_EQ(1, CountSum(123, 6));
+    EXPECT_EQ(1, CountSum(9101, 11));
 }
 
-TEST(PrintNumberTest, Zero) {
-    testing::internal::CaptureStdout();
-    PrintNumber(0, 14);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(4, 0);
-    std::string output2 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output2, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(0, 0);
-    std::string output3 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output3, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
+TEST(CountSumTest, Incorrect) {
+    EXPECT_EQ(0, CountSum(7853, 13));
+    EXPECT_EQ(0, CountSum(43, 9));
 }
 
-TEST(PrintNumberTest, Double) {
-    testing::internal::CaptureStdout();
-    PrintNumber(2.3, 12);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(3.5, 2.4);
-    std::string output2 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output2, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(6, 7.1);
-    std::string output3 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output3, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
+TEST(ReadNumberTest, Correct) {
+    EXPECT_EQ(28, ReadNumber(3, 6));
 }
 
-TEST(PrintNumberTest, Words) {
-    testing::internal::CaptureStdout();
-    PrintNumber('cbe', 9);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber('dn', 'd');
-    std::string output2 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output2, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
-
-    testing::internal::CaptureStdout();
-    PrintNumber(4, 'dk');
-    std::string output3 = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output3, "¬ведите корректные данные: <значность сумма> (целые положительные числа больше 0)\n");
+TEST(ReadNumberTest, Incorrect) {
+    EXPECT_EQ(0, ReadNumber(1.2, 4));
+    EXPECT_EQ(0, ReadNumber('hd', 'j'));
 }
 
 int main(int argc, char** argv) {
