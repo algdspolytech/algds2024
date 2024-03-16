d9116a32f3ef3790bcc4fe0882cbd63ea94cb1b2
1Sorangar1 <ragnarchern@mail.ru>
2024-03-16 15:47:42 +0300

    readfileTest

--- lab1/first_lab/main.c
+++ lab1/first_lab/main.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-//#include <cassert>
 
 typedef struct list_s {
 	int* seq;
@@ -17,7 +16,7 @@ list_s* readfile(char const* filename) {
 	f = fopen(filename, "r");
 	if (f == NULL) {
 		printf("Errors occured while reading file");
-		exit(0);
+		exit(1);
 	}
 
 	int n;
@@ -25,8 +24,9 @@ list_s* readfile(char const* filename) {
 
 	if (n == 0) {
 		printf("No sequence is given");
-		exit(0);
+		exit(2);
 	}
+
 	int* list = (int*)malloc(n * sizeof(int));
 	for (int i = 0; i < n; i++) {
 		fscanf(f, "%d", &list[i]);
@@ -77,7 +77,74 @@ list_s* function(int const* list, int const n) {
 	return result;
 }
 
+
+void readfileTest() {
+
+	list_s* test1 = (list_s*)malloc(sizeof(list_s));
+	test1->N = 3;
+	int* list1 = (int*)malloc(3 * sizeof(int));
+	list1[0] = 1;
+	list1[1] = 2;
+	list1[2] = 3;
+	test1->seq = list1;
+	list_s* test1t = readfile("test1.txt");
+	if (test1->N != test1t->N) {
+		printf("test1 Error");
+		exit(1);
+	}
+	for (int i = 0; i < 3; i++) {
+		if (test1->seq[i] != test1t->seq[i]) {
+			printf("test1 Error");
+			exit(1);
+		}
+	}
+
+	list_s* test2 = (list_s*)malloc(sizeof(list_s));
+	test2->N = 4;
+	int* list2 = (int*)malloc(4 * sizeof(int));
+	list2[0] = 1;
+	list2[1] = 2;
+	list2[2] = 2;
+	list2[3] = 1;
+	test2->seq = list2;
+	list_s* test2t = readfile("test2.txt");
+	if (test2->N != test2t->N) {
+		printf("test2 Error");
+		exit(1);
+	}
+	for (int i = 0; i < 4; i++) {
+		if (test2->seq[i] != test2t->seq[i]) {
+			printf("test2 Error");
+			exit(1);
+		}
+	}
+
+
+	list_s* test3 = (list_s*)malloc(sizeof(list_s));
+	test3->N = 1;
+	int* list3 = (int*)malloc(sizeof(int));
+	list3[0] = 8;
+	test3->seq = list3;
+	list_s* test3t = readfile("test3.txt");
+	if (test3->N != test3t->N) {
+		printf("test3 Error");
+		exit(1);
+	}
+	for (int i = 0; i < 1; i++) {
+		if (test3->seq[i] != test3t->seq[i]) {
+			printf("test3 Error");
+			exit(1);
+		}
+	}
+
+	printf("readfileTest OK\n");
+}
+
+
 int main() {
+
+	readfileTest();
+
 	list_s* list = readfile("input.txt");
 
 	list_s* result = function(list->seq, list->N);
