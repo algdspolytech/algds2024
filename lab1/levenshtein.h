7b40599d468cd629e7687c757362800a3fa6499a
ALEXkonkova <73539963+ALEXkonkova@users.noreply.github.com>
2024-03-17 22:32:26 +0300

    Update levenshtein.h

--- lab1/levenshtein.h
+++ lab1/levenshtein.h
@@ -1,9 +1,9 @@
 #ifndef LEVENSHTEIN_H
 #define LEVENSHTEIN_H
-
+#include <stdio.h>
 #include <wchar.h>
 
 int minimum(int a, int b, int c);
 int levenshteinDistance(wchar_t* x, int m, wchar_t* y, int n);
 
-#endif // LEVENSHTEIN_H
+#endif
