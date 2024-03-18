96661f46aaaf5fb6047c47e7f09061dc7a50f627
Mouser5 <stanislavegorkin7@gmail.com>
2024-03-18 04:10:16 +0300

    Create Find_Diagonals

--- lab1/[5030102_20001]_Egorkin_lab1/Sourse/Sourse.c
+++ lab1/[5030102_20001]_Egorkin_lab1/Sourse/Sourse.c
@@ -3,6 +3,40 @@
 #include <stdlib.h>
 #include <math.h>
 
+double Diagonal_Value(int x1, int x2, int y1, int y2) {
+	double value = sqrt((pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+	return(value);
+}
+
+double* Find_Diagonal(const int** coord, const int size) {
+	double** diagonals;
+	double* Ans = (double*)malloc((size - 3) * sizeof(double));
+	double Max_diagonal = 0, Min_diagonal = -1;
+	diagonals = (double**)malloc((size) * sizeof(double));
+	for (int i = 0; i < size; i++) {
+		diagonals[i] = (double*)malloc((size - 3) * sizeof(double));
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size - 3; j++) {
+			diagonals[i][j] = Diagonal_Value(coord[i][0], coord[(i + 2 + j) % size][0], coord[i][1], coord[(i + 2 + j) % size][1]);
+			if (Max_diagonal < diagonals[i][j])
+				Max_diagonal = diagonals[i][j];
+		}
+		if ((Min_diagonal > Max_diagonal) || (Min_diagonal == -1)) {
+			Min_diagonal = Max_diagonal;
+			for (int temp = 0; temp < size - 3; temp++) {
+				Ans[temp] = diagonals[i][temp];
+			}
+		}
+		Max_diagonal = 0;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		free(diagonals[i]);
+	}
+	free(diagonals);
+	return(Ans);
+}
 
 
 int Solution(int* Coord[2], int n) {
