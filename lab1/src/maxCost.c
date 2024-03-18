54ded7cd623e5c99b3ea11791619d346a18b9d9f
BKUD <buuchaysatunov@gmail.com>
2024-03-19 00:53:01 +0300

    update test.cpp and maxCost.cpp

--- lab1/src/maxCost.c
+++ lab1/src/maxCost.c
@@ -1,7 +1,7 @@
 #include "maxCost.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 
 int _max(int a, int b, int c) {
     if (a > b && a > c) return a;
@@ -29,14 +29,14 @@ int maxCost(int **cost, int N) {
 
     for (int i = 1; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            int upLeft = (j > 0) ? dp[i - 1][j - 1] : 0;
-            int upRight = (j < N - 1) ? dp[i - 1][j + 1] : 0;
+            int upLeft = (j > 0) ? dp[i - 1][j - 1] : INT_MIN;
+            int upRight = (j < N - 1) ? dp[i - 1][j + 1] : INT_MIN;
             int up = dp[i - 1][j];
             dp[i][j] = cost[i][j] + _max(upLeft, upRight, up);
         }
     }
 
-    int result = 0;
+    int result = INT_MIN;
     for (int i = 0; i < N; i++)
         if (dp[N - 1][i] > result)
             result = dp[N - 1][i];
--- lab1/test/test.cpp
+++ lab1/test/test.cpp
@@ -25,13 +25,26 @@ TEST(MaxCostTest, TestTwoByTwoBoard) {
         cost[i] = (int*)malloc(N * sizeof(int));
     cost[0][0] = 1; cost[0][1] = 2;
     cost[1][0] = 3; cost[1][1] = 4;
-    EXPECT_EQ(maxCost(cost, N), 7);
+    EXPECT_EQ(maxCost(cost, N), 6);
     for (int i = 0; i < N; i++)
         free(cost[i]);
     free(cost);
 }
 
-TEST(MaxCostTest, TestThreeByThreeBoard) {
+TEST(MaxCostTest, TestThreeByTwoBoard) {
+    int N = 2;
+    int** cost = (int**)malloc(N * sizeof(int*));
+    for (int i = 0; i < N; i++)
+        cost[i] = (int*)malloc(N * sizeof(int));
+    cost[0][0] = -1; cost[0][1] = -2;
+    cost[1][0] = -3; cost[1][1] = -4;
+    EXPECT_EQ(maxCost(cost, N), -4);
+    for (int i = 0; i < N; i++)
+        free(cost[i]);
+    free(cost);
+}
+
+TEST(MaxCostTest, TestFourByThreeBoard) {
     int N = 3;
     int** cost = (int**)malloc(N * sizeof(int*));
     for (int i = 0; i < N; i++)
@@ -39,13 +52,13 @@ TEST(MaxCostTest, TestThreeByThreeBoard) {
     cost[0][0] = 1; cost[0][1] = 2; cost[0][2] = 3;
     cost[1][0] = 4; cost[1][1] = 5; cost[1][2] = 6;
     cost[2][0] = 7; cost[2][1] = 8; cost[2][2] = 9;
-    EXPECT_EQ(maxCost(cost, N), 23);
+    EXPECT_EQ(maxCost(cost, N), 18);
     for (int i = 0; i < N; i++)
         free(cost[i]);
     free(cost);
 }
 
-TEST(MaxCostTest, TestFurByThreeBoard2) {
+TEST(MaxCostTest, TestFiveByThreeBoard2) {
     int N = 3;
     int** cost = (int**)malloc(N * sizeof(int*));
     for (int i = 0; i < N; i++)
@@ -53,7 +66,7 @@ TEST(MaxCostTest, TestFurByThreeBoard2) {
     cost[0][0] = 7; cost[0][1] = 8; cost[0][2] = 90;
     cost[1][0] = 1; cost[1][1] = 1; cost[1][2] = 87;
     cost[2][0] = 7; cost[2][1] = 8; cost[2][2] = 1;
-    EXPECT_EQ(maxCost(cost, N), 23);
+    EXPECT_EQ(maxCost(cost, N), 185);
     for (int i = 0; i < N; i++)
         free(cost[i]);
     free(cost);
