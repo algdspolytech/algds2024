573399bb0e6469bbf05f1f5bd4c46416239736b3
BKUD <buuchaysatunov@gmail.com>
2024-03-19 00:23:53 +0300

    Added maxCost.c and maxCost.h and update main.c

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -1,52 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
-int max(int a, int b, int c) {
-    if (a > b && a > c) return a;
-    if (b > a && b > c) return b;
-    return c;
-}
-
-int maxCost(int** cost, int N) {
-    int** dp = (int**)malloc(N * sizeof(int*));
-    if (dp == NULL) {
-        printf("Ошибка выделения памяти\n");
-        return -1;
-    }
-
-    for (int i = 0; i < N; i++) {
-        dp[i] = (int*)malloc(N * sizeof(int));
-        if (dp[i] == NULL) {
-            printf("Ошибка выделения памяти\n");
-            return -1;
-        }
-    }
-
-    for (int i = 0; i < N; i++)
-        dp[0][i] = cost[0][i];
-
-    for (int i = 1; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            int upLeft = (j > 0) ? dp[i - 1][j - 1] : 0;
-            int upRight = (j < N - 1) ? dp[i - 1][j + 1] : 0;
-            int up = dp[i - 1][j];
-            dp[i][j] = cost[i][j] + max(upLeft, upRight, up);
-        }
-    }
-
-    int result = 0;
-    for (int i = 0; i < N; i++)
-        if (dp[N - 1][i] > result)
-            result = dp[N - 1][i];
-
-    for (int i = 0; i < N; i++)
-        free(dp[i]);
-    free(dp);
-
-    return result;
-}
+#include "maxCost.h"
 
 int main() {
     int N;
@@ -55,14 +9,14 @@ int main() {
         return 1;
     }
 
-    int** cost = (int**)malloc(N * sizeof(int*));
+    int **cost = (int **) malloc(N * sizeof(int *));
     if (cost == NULL) {
         printf("Ошибка выделения памяти\n");
         return 1;
     }
 
     for (int i = 0; i < N; i++) {
-        cost[i] = (int*)malloc(N * sizeof(int));
+        cost[i] = (int *) malloc(N * sizeof(int));
         if (cost[i] == NULL) {
             printf("Ошибка выделения памяти\n");
             return 1;
--- lab1/test/test.cpp
+++ lab1/test/test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "main.h" 
+#include "maxCost.h"
 
 TEST(MaxCostTest, TestEmptyBoard) {
     int N = 0;
@@ -39,6 +39,20 @@ TEST(MaxCostTest, TestThreeByThreeBoard) {
     cost[0][0] = 1; cost[0][1] = 2; cost[0][2] = 3;
     cost[1][0] = 4; cost[1][1] = 5; cost[1][2] = 6;
     cost[2][0] = 7; cost[2][1] = 8; cost[2][2] = 9;
+    EXPECT_EQ(maxCost(cost, N), 23);
+    for (int i = 0; i < N; i++)
+        free(cost[i]);
+    free(cost);
+}
+
+TEST(MaxCostTest, TestFurByThreeBoard2) {
+    int N = 3;
+    int** cost = (int**)malloc(N * sizeof(int*));
+    for (int i = 0; i < N; i++)
+        cost[i] = (int*)malloc(N * sizeof(int));
+    cost[0][0] = 7; cost[0][1] = 8; cost[0][2] = 90;
+    cost[1][0] = 1; cost[1][1] = 1; cost[1][2] = 87;
+    cost[2][0] = 7; cost[2][1] = 8; cost[2][2] = 1;
     EXPECT_EQ(maxCost(cost, N), 23);
     for (int i = 0; i < N; i++)
         free(cost[i]);
