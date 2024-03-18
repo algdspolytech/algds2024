d572fd3e347b61aa870d896a9abb4ee2ba987ec4
Hukker <54433332+Hukker@users.noreply.github.com>
2024-03-19 00:29:32 +0300

    lab 1 v13 version with tests

--- lab1/source.c
+++ lab1/source.c
@@ -1,27 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 #define N 7
 
+#define M 13
 //#define ERROR_OPEN_FILE -1
 
 #define CREATE_ERROR -2
 
 typedef int u32;
 
-u32* create_a(u32 *num)
+u32* create_a(u32 num)
 { 
-    if (*num == 0) {
-        printf("there must be 2 numbers at least");
-        exit(1);
+    if (num == 0 || num == 1) {
+         printf("there must be 2 numbers at least");
+         exit(1);
     }
-    if (*num == 1) {
-        printf("there are must be more then 1 number");
-        exit(1);
-    }
-    u32* a = malloc(*num * sizeof(u32));
-    for (int i = 0; i < *num; i++)
+   
+    u32* a = malloc(num * sizeof(u32));
+    for (int i = 0; i < num; i++)
     {
         scanf("%d", &a[i]);
         if (a[i] == " " || a[i] == '\0') {
@@ -32,18 +31,18 @@ u32* create_a(u32 *num)
 }
 
 
-void proccessing(u32* a, int *n) 
+void proccessing(u32* a, int n) 
 {
-    u32 max = 1, index_max = 1;
-    u32* c = (u32*)malloc(sizeof(u32) * (*n + 1));
-    u32* b = (u32*)malloc(sizeof(u32) * (*n + 1));
-    for (int i = 2; i < *n+1; i++) {
+    u32 max = -1, index_max = 0;
+    u32* c = (u32*)malloc(sizeof(u32) * (n));
+    u32* b = (u32*)malloc(sizeof(u32) * (n));
+    for (int i = 0; i < n; i++) {
         b[i] = 0;
     }
-    b[1] = 1;
+    c[0] = -1;
     c[1] = 0;
-    for (int i = 2; i <= *n; i++) {
-        for (int j = 1; j < i; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
             if (a[j] < a[i] && b[i] < b[j] + 1) {
                 c[i] = j;
                 b[i] = b[j] + 1;
@@ -56,24 +55,62 @@ void proccessing(u32* a, int *n)
     }
    
     printf("%d\n", a[index_max]);
-    do {
+    while (index_max != 0) {
+
         index_max = c[index_max];
         printf("%d\n", a[index_max]);
-    } while (index_max != 0);
+
+    }
+   
 }
 
-void test_proccwssing(int n) 
+void tests()
 {
-    u32* a = create_a(n);
-    proccessing(a, n);
+    printf("1. Тест 'в массиве 1 элемент' \n");
+    u32 m = 1;
+    u32* a = malloc(m * sizeof(u32));
+    a[0] = 1;
+    proccessing(a, m);
+    free(a);
+    printf("\n");
+
+    printf("2. Обратный порядок элементов\n");
+    m = 5;
+    a = malloc(m * sizeof(u32));
+    printf("массив = ");
+    for (u32 i = 0; i < m; i++)
+    {
+        a[i] = 5 - i;
+        printf("%d ", a[i]);
+    }
+
+    proccessing(a, m);
+    free(a);
+    printf("\n");
+
+    printf("3. Тест на две подпоследовательности разной длины\n");
+    //M = 13;
+    u32 b[M] = { 1,2,2,2,4,5,6,3,4,7,8,9,6 };
+    printf("Массив = {1,2,2,2,4,5,6,3,4,7,8,9,6}\n");
+    proccessing(b,m);
+    printf("\n");
+
 }
 
 u32 main() 
 {
+    setlocale(LC_CTYPE, "Rus");
     int n = 5;
-    u32 * a = create_a(&n);
     printf("\n\n\n");
-    proccessing(a, &n);
+    tests();
+    printf("\n\n\n");
+    puts("enter ur numbres");
+    u32* a = create_a(n);
+
+    printf("\n\n\n");
+    puts("ur answer:\n");
+    proccessing(a, n);
+    
     free(a);
     return 0;
 }
