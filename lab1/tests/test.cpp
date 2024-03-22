6f107353120edcbcc444bbebd7806a35caf5b69c
Ruslan-Tr <163584657+Ruslan-Tr@users.noreply.github.com>
2024-03-22 23:39:29 +0300

    Update test.cpp

--- lab1/tests/test.cpp
+++ lab1/tests/test.cpp
@@ -2,54 +2,33 @@
 #include "main.h"
 
 TEST(test, t1) {
-    Point t1 = {0, 0}, t2 = {0, 5};
-    float answer = 5.0f;
-    ASSERT_EQ(answer, Distance(t1, t2));
+    const int N = 5, A = 10;
+    int weights[N] = {1, 2, 3, 4, 4};
+    int values[N] = {2, 4, 6, 8, 10};
+    int answer = 22;
+    ASSERT_EQ(answer, Result(N, A, weights, values));
 }
 
 TEST(test, t2) {
-    Point t3 = {-1, 6}, t4 = {-1, -4};
-    float answer = 10.0f;
-    ASSERT_EQ(answer, Distance(t3, t4));
+    const int N = 6, A = 13;
+    int weights[N] = {2, 3, 4, 6, 5, 1};
+    int values[N] = {5, 1, 4, 6, 3, 8};
+    int answer = 23;
+    ASSERT_EQ(answer, Result(N, A, weights, values));
 }
 
 TEST(test, t3) {
-    int N = 25;
-    int j = N - 1, i = 0;
-    float answer = 0.f;
-    ASSERT_EQ(answer, Check(i, j, N));
+    const int N = 8, A = 11;
+    int weights[N] = {2, 1, 3, 5, 1, 8, 3, 4};
+    int values[N] = {3, 1, 2, 5, 4, 1, 2, 3};
+    int answer = 14;
+    ASSERT_EQ(answer, Result(N, A, weights, values));
 }
 
 TEST(test, t4) {
-    int N = 6;
-    int j = 0, i = N - 1;
-    float answer = 0;
-    ASSERT_EQ(answer, Check(i, j, N));
-}
-
-TEST(test, t5) {
-    int pos_point;
-    const int N = 5;
-    Point arr[N] = {{0, 0},
-                    {2, 0},
-                    {3, 0.8},
-                    {4, 2},
-                    {1, 3}};
-    float answer = 2;
-    Result(&pos_point, arr, N);
-    ASSERT_EQ(answer, pos_point);
-}
-
-TEST(test, t6) {
-    int pos_point;
-    const int N = 6;
-    Point arr[N] = {{-1, -1},
-                    {1,  -1},
-                    {2,  0},
-                    {1,  2},
-                    {-1, 3},
-                    {-2, 1}};
-    float answer = 3;
-    Result(&pos_point, arr, N);
-    ASSERT_EQ(answer, pos_point);
+    const int N = 3, A = 3;
+    int weights[N] = {2, 3, 2};
+    int values[N] = {10, 6, 13};
+    int answer = 13;
+    ASSERT_EQ(answer, Result(N, A, weights, values));
 }
